bf59cea96b6a71a27ffa6ad7cf355774216d5ceb
tjtyson1 <tjupnext@icloud.com>
2025-09-02 14:52:40 -0400

    Add files via upload

--- Simple-Linear-Regression.R
+++ Simple-Linear-Regression.R
@@ -5,3 +5,15 @@ cars <- mtcars
 
 # Scatter plot on training data
 scatter.smooth(x=cars$mpg, y =cars$disp, main = "SpeedVSdistance")
+
+#Calculate correlation value between mpg and disp
+cor(cars$mpg, cars$disp)
+
+# - dim() tells you the columns and rows the data has
+
+# Split training and testing data for regression
+training_data <- cars[1:16,] 
+# - comma at end selects rows comma at beginning selects rows
+test_data <- cars[17:32,]
+
+# Build a regression model
