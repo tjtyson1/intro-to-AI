45669dc1d7943b164981afa42a91ce0b8069c1ee
tjtyson1 <tjupnext@icloud.com>
2025-09-04 14:38:28 -0400

    Add files via upload

--- Simple-Linear-Regression.R
+++ Simple-Linear-Regression.R
@@ -17,3 +17,13 @@ training_data <- cars[1:16,]
 test_data <- cars[17:32,]
 
 # Build a regression model
+regression_result <- lm(mpg ~ disp, data=training_data)
+# lm means linear model
+
+# Predict regression function on test data
+prediction_result <- predict(regression_result, test_data)
+
+#Results of prediction
+actual_prediction_values <- data.frame(cbind(actuals=training_data$mpg, predicteds=prediction_result))
+
+
